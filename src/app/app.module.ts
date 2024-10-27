9deaa8b7d5046812c2cbfe0bf6568eee270c3551
Kamile Pimenta <kamile.pimenta@outlook.com>
2024-10-27 15:53:42 -0300

    feat: prepare custom web component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
+import { CartComponent } from './pages/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor( private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const childAppComponent = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('kms-cart', childAppComponent);
+  }
+}
